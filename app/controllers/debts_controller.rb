e7ba5b0b63956e2555905c53ff682bd6337c2db3
Tiffany <Tafby88@gmail.com>
2019-04-13 16:05:44 -0700

    added crud inside controllers

--- app/controllers/debts_controller.rb
+++ app/controllers/debts_controller.rb
@@ -1,2 +1,17 @@
 class DebtsController < ApplicationController
+  def create
+  debt = Debt.create(debt_params(params))
+  render json: debt
+  end
+
+  def show
+  debt = Debt.find(params[:id])
+  render json: debt
+  end
+
+  private 
+
+  def debt_params(params) 
+    params.require(:debt).permit(:type, :amount, :interest_rate, :user_id, :min_payment)
+  end
 end
--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,2 +1,18 @@
 class PlansController < ApplicationController
+  def create
+  plan = Plan.create(plan_params(params))
+  render json: plan
+  end
+
+  def show
+  plan = Plan.find(params[:id])
+  render json: plan
+  end
+
+  private 
+
+  def plan_params(params) 
+    params.require(:plan).permit(:user_id, :name)
+  end
+  
 end
