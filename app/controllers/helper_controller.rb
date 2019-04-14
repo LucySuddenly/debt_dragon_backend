3175a98618a4ef6818f6fae38336f284eec10ba9
Tiffany <Tafby88@gmail.com>
2019-04-13 17:54:47 -0700

    added returnPlan function

--- app/controllers/helper_controller.rb
+++ app/controllers/helper_controller.rb
@@ -5,4 +5,16 @@ class HelperController < ApplicationController
     debts = Debts.find_by(user_id: user.id)
     render json: threePlans(user)
   end  
+
+  def returnPlan
+    user = User.find_by(params[:id])
+    if user.plan.name == "avalanche" 
+      returnValue = generatePlan(avalanche(user), user)
+    elsif user.plan.name == "snowball" 
+      returnValue = generatePlan(snowball(user), user)
+    elsif user.plan.name == "credit"
+      returnValue = generatePlan(credit(user), users)
+    end
+    returnValue
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :plans
   resources :users
   post "/selectPlan", to: "helper#selectPlan" 
+  post "/returnPlan", to: "helper#returnPlan"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
