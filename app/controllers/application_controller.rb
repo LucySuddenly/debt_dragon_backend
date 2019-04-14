8ab92f789a4b1b8f2689fc137eb8460258b84a5e
Lucy Suddenly <lucysuddenly@Lucys-MacBook-Air.local>
2019-04-13 22:47:48 -0700

    removed puts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
     debts = Debt.all
     debts = debts.select{|debt| debt.user_id == user.id}
     debts.sort_by! { |debt| debt.amount }
-    
+
   end
 
   def credit(user)
@@ -23,18 +23,17 @@ class ApplicationController < ActionController::API
 
   def monthsTillPayoff(amount, targetPayment, mpr)
       months = 0
-      while amount > 0 
+      while amount > 0
         amount -= (targetPayment - (amount * mpr))
         months += 1
       end
       return months
     end
 
-  def generatePlan(debts, user) 
+  def generatePlan(debts, user)
       returnArray = []
-      puts debts.length
       returnHash = {}
-       debts.length.times do 
+       debts.length.times do
         returnHash.clear
         mpr = debts.first.interest_rate / 1200
         min_total = 0
@@ -44,13 +43,12 @@ class ApplicationController < ActionController::API
         targetPayment = user.income - user.expenses - min_total + debts.first.min_payment
         months = monthsTillPayoff(debts.first.amount, targetPayment, mpr)
         returnHash["targetPayment"] = targetPayment
-        returnHash["months"] = months 
+        returnHash["months"] = months
         returnHash["amount"] = debts.first.amount
         returnHash["debt_type"] = debts.first.debt_type
         returnHash["mpr"] = mpr
-
         newReturnHash = returnHash.clone
-        returnArray.insert(-1, newReturnHash) 
+        returnArray.insert(-1, newReturnHash)
         debts.shift
       end
       returnArray
